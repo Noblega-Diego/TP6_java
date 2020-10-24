8a6fb5a2960d9f5ed48132cead63ece4b20c8f79
diego <diego@laptop-swift3.com>
2020-10-23 23:34:25 -0300

    tp terminado

--- src/tp6_java/ejercicio03/MenuRestaurant.java
+++ src/tp6_java/ejercicio03/MenuRestaurant.java
@@ -3,6 +3,7 @@ package tp6_java.ejercicio03;
 import java.util.ArrayList;
 import java.util.Scanner;
 import utiles.Lectura;
+import utiles.Utiles;
 public class MenuRestaurant {
     public static void main(String[] args) {
         ArrayList<Plato>platos= new ArrayList<Plato>();
@@ -15,12 +16,8 @@ public class MenuRestaurant {
             boolean esBebida;
             System.out.println("-------------------------");
             nombre = Lectura.leerStringM("ingrese nombre del plato:");
-            String bebida = Lectura.leerStringM("¿es una Bebida? \n ingrese 'y' para SI \n ó cualquier otra letra para NO\n");
-            if(bebida.equalsIgnoreCase("y")){
-                esBebida = true;
-            }else {
-                esBebida = false;
-            }
+            String bebida = Lectura.leerStringM("¿es una Bebida? \ningrese 'y' para SI \nó cualquier otra letra para NO\n ingrese:");
+            esBebida = bebida.equalsIgnoreCase("y");
             precio = Lectura.leerDoubleM("ingrese el precio:");
             plato.setNombreCompleto(nombre);
             plato.setEsBebida(esBebida);
@@ -39,31 +36,40 @@ public class MenuRestaurant {
                 ingrediente.setNombre(nombreIngrediente);
                 ingrediente.setUnidadMedida(medida);
                 plato.addIngrediente(ingrediente);
-                if(!continuarM("¿desea continuar añadiendo ingredientes?"))
+                if(!Utiles.continuarM("¿desea continuar añadiendo ingredientes?"))
                     break;
             }
             platos.add(plato);
 
-        }while (continuarM("¿desea continuar añadiendo platos?"));
+        }while (Utiles.continuarM("¿desea continuar añadiendo platos?"));
+
+        mostrarMenu(platos);
     }
 
     private static void mostrarMenu(ArrayList<Plato> menu){
-        System.out.println("-----------MENÚ----------------");
+        System.out.println("----------------MENÚ----------------");
         for (Plato plato: menu) {
             System.out.println(plato.getNombreCompleto());
-            System.out.println("$"+plato.getPrecio());
-            System.out.println("ingredientes:");
+            System.out.println("precio: $ "+plato.getPrecio());
+            if(!plato.isEsBebida()){
+                System.out.println("ingredientes:");
+                System.out.println(Utiles.rellenarEspaciosSimple("nombre",18) +
+                        Utiles.rellenarEspaciosSimple("cantidad",9) + "unidad");
+                for (Ingrediente ingrediente: plato.getIngredientes()) {
+                    System.out.println(Utiles.rellenarEspaciosSimple(ingrediente.getNombre(),18) +
+                            Utiles.rellenarEspaciosSimple(String.valueOf(ingrediente.getCantidad()),9) + ingrediente.getUnidadMedida());
+                }
+            }
+            System.out.println("------------------------------------");
         }
+        System.out.println("TOTAL DEL MENU: $ "+ totalDelMenu(menu));
     }
 
-    private static boolean continuarM(String mensaje){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(mensaje +" (Y) ó cualquier otra letra para salir");
-        String next = sc.nextLine();
-        if(next.equalsIgnoreCase("y")){
-            return true;
-        }else {
-            return false;
+    private static double totalDelMenu(ArrayList<Plato> menu){
+        double total = 0;
+        for (Plato plato:menu) {
+            total += plato.getPrecio();
         }
+        return total;
     }
 }
